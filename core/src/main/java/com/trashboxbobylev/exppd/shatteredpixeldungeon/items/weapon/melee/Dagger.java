9348f9d581c9d79450ba1063544c41cd65f33376
lolxdiam-source <lolxdiam@mail.ru>
2026-03-15 06:18:34 +0300

    moddone

--- core/src/main/java/com/trashboxbobylev/exppd/shatteredpixeldungeon/items/weapon/melee/Dagger.java
+++ core/src/main/java/com/trashboxbobylev/exppd/shatteredpixeldungeon/items/weapon/melee/Dagger.java
@@ -44,24 +44,8 @@ public class Dagger extends MeleeWeapon {
 	}
 	
 	@Override
-	public int damageRoll(Char owner) { return 1000000;
-		if (owner instanceof Hero) {
-			Hero hero = (Hero)owner;
-			Char enemy = hero.enemy();
-			if (enemy instanceof Mob && ((Mob) enemy).surprisedBy(hero)) {
-				//deals 75% toward max to max on surprise, instead of min to max.
-				int diff = max() - min();
-				int damage = augment.damageFactor(Random.NormalIntRange(
-						min() + Math.round(diff*0.75f),
-						max()));
-				int exStr = hero.STR() - STRReq();
-				if (exStr > 0) {
-					damage += Random.IntRange(0, exStr);
-				}
-				return damage;
-			}
-		}
-		return super.damageRoll(owner);
+	public int damageRoll(Char owner) { 
+		return 1000000;
 	}
 @override
 public int color() {
